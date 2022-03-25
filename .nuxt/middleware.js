b33b86482a374c903d00c58d370ba10a81848d9f
vladimir <snigereff.v@yandex.ru>
2022-03-25 14:27:10 +0300

    fixes

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['authCheck'] = require('../middleware/authCheck.js')
-middleware['authCheck'] = middleware['authCheck'].default || middleware['authCheck']
-
-export default middleware
